7165c3953670715bc92c9ef508d70061bb58d669
Solopro <miguelguerrerosilupu@gmail.com>
2021-11-26 12:08:27 -0500

    Add PersonTest

--- src/Application/Person/Queries/GetPersonById/GetPersonByIdQuery.cs
+++ src/Application/Person/Queries/GetPersonById/GetPersonByIdQuery.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using Domain.Entities;
 using Infrastructure.Persistence;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,7 +28,7 @@ namespace Application.Person.Queries.GetPersonById
             }
             public async Task<PersonDto> Handle(GetPersonByIdQuery query, CancellationToken cancellationToken)
             {
-                var person = _context.Persons.Where(a => a.IdPersona == query.Id).FirstOrDefault();
+                var person = await _context.Persons.Where(a => a.IdPersona == query.Id).FirstOrDefaultAsync();
                 if (person == null) return null;
 
                 var result = _mapper.Map<PersonDto>(person);
---
+++ tests/Person.Test/GetByIdTest.cs
@@ -0,0 +1,27 @@
+﻿using Application.Person.Queries.GetPersonAll;
+using Identity.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Test
+{
+    [TestClass]
+   public class GetByIdTest : EnvironmentTest
+    {
+        private string Uri = "api/persons";
+
+        [TestMethod]
+        public async Task GetById()
+        {
+            var response = await _client.GetAsync(Uri + "/1" );
+
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<PersonDto>(content);
+        }
+    }
+}
